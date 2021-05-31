8d71df0e628391453b0b9fb91111760e9591c947
Josh Longton <longton95@gmail.com>
2021-06-01 00:39:10 +0100

    added comparison to time

--- client/src/panels/LeftPanel.js
+++ client/src/panels/LeftPanel.js
@@ -2,21 +2,27 @@ import React from "react";
 import { getTime } from "../services/time";
 import moment from 'moment'
 
-
+// var diff
 
 
 function LeftPanel() {
 
-	const [serverTim, setData] = React.useState(null);
-	let localDate = Date.now()
-	let serverTime
+	const [localDate, setlocalDate] = React.useState(null);
+	const [serverTime, setserverTime] = React.useState(null);
+
+
 
 	React.useEffect(() => {
+		setlocalDate(Date.now());
+		getTime().then(data => setserverTime(data.epoch));
 
-		getTime().then(data => setData(data.epoch));
+		setInterval(() => {
+			setlocalDate(Date.now());
+		}, 1000)
+
+		getTime().then(data => setserverTime(data.epoch));
 		const interval = setInterval(() => {
-			getTime().then(data => setData(data.epoch));
-			localDate = Date.now()
+			getTime().then(data => setserverTime(data.epoch));
 		}, 30000)
 
 		return () => clearInterval(interval)
@@ -24,15 +30,14 @@ function LeftPanel() {
 	}, []);
 
 	return (
-
 		<div className='splitScreen'>
-			<div style={serverTime ? {} : { display: 'none' }}>
+			<div>
 				<h2>Time</h2>
 				<p>Server epoch: {serverTime}</p>
 				<p>local epoch: {localDate}</p>
 				<p>serverTime: {moment(serverTime).format("hh:mm:ss")}</p>
 				<p>localtime: {moment(localDate).format("hh:mm:ss")}</p>
-				<p>difference: {moment(localDate - serverTime).format("hh:mm:ss")}</p>
+				<p>difference: {moment.utc(moment(moment(localDate), "DD/MM/YYYY HH:mm:ss").diff(moment(moment(serverTime), "DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss")}</p>
 			</div>
 		</div>
 	);
