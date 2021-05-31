9c1db494d17f2a6d55a0c4218aa37b7956252489
Josh Longton <longton95@gmail.com>
2021-06-01 00:40:11 +0100

    removed commented code

--- client/src/App.js
+++ client/src/App.js
@@ -27,11 +27,3 @@ function App() {
 }
 
 export default App;
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>{!data ? "Loading..." : data}</p>
-    //   </header>
-    // </div>
