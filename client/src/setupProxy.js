efad16bdee6364af1936c4969642214392d5cbe2
Josh Longton <longton95@gmail.com>
2021-06-01 02:51:27 +0100

    Dockerise app

---
+++ client/src/setupProxy.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+	app.use(
+		'/time',
+		createProxyMiddleware({
+			target: 'http://backend:3001',
+			changeOrigin: true,
+		})
+	);
+	app.use(
+		'/metrics',
+		createProxyMiddleware({
+			target: 'http://backend:3001',
+			changeOrigin: true,
+		})
+	);
+};
